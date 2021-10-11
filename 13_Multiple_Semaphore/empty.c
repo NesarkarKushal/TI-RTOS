c4aec3fc00b6fcfd1b034bd10b8779b44877e7b6
NesarkarKushal <kushalnesarkar@gmail.com>
2021-10-11 10:54:48 +0530

    Creating Multiple Semaphore

---
+++ 13_Multiple_Semaphore/empty.c
@@ -0,0 +1,71 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Semaphore.h>
+#include <ti/drivers/GPIO.h>
+
+#include "Board.h"
+//# define Timeout 1000
+
+extern Semaphore_Handle  semaphore0;
+extern Semaphore_Handle  semaphore1;
+
+
+Void Task_A(UArg arg0, UArg arg1)
+{
+    Semaphore_post(semaphore0);
+    Task_sleep(1000);
+}
+
+Void Task_B(UArg arg0, UArg arg1)
+{
+    while(1)
+    {
+    Semaphore_pend(semaphore0,BIOS_WAIT_FOREVER);
+
+    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x20);
+    SysCtlDelay(13333333);
+    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x00);
+    SysCtlDelay(13333333);
+
+    Semaphore_post(semaphore1);
+    Task_sleep(1000);
+    }
+
+}
+
+Void Task_C(UArg arg0, UArg arg1)
+{
+    while(1)
+    {
+    Semaphore_pend(semaphore1,BIOS_WAIT_FOREVER);
+    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x40);
+    SysCtlDelay(13333333);
+    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x00);
+    SysCtlDelay(13333333);
+    }
+}
+
+int main(void)
+{
+
+    Board_initGeneral();
+    Board_initGPIO();
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE ,0xF0);
+
+    BIOS_start();
+
+    return (0);
+}
