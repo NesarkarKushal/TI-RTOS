2de24751595885573e6419b67bc70ae9b2e55177
NesarkarKushal <86586956+NesarkarKushal@users.noreply.github.com>
2021-11-03 10:25:00 +0530

    Creating Task Thread Statically

--- 03_TASK_THREAD_Static/empty.c
+++ 03_TASK_THREAD_Static/empty.c
@@ -5,9 +5,9 @@
      About Using Idle Thread:
 
                   Task thread is 2nd lowest priority thread in TI-RTOS and runs after Highest priority thread.
-                  Task thread will executes only once if there is no while(1). So if you want to run for infinite
-                  times then we should go for while(1) loop.
-                  In this program there is only one Task Thread.
+                  Task thread will executes only once if there is no while(1) or any infinite loop. So if you want to run for infinite
+                  times then we should go for while(1) or any infinite loop.
+                  In this program there is only one Task Thread which will execute continuously ( Blinking of LED ) 
 */
 
 #include <stdint.h>
@@ -36,10 +36,10 @@ Void Task_A(UArg arg0, UArg arg1)
 {
     while(1)
     {
-    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x20)   ;
-    SysCtlDelay(13333333);
-    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x00);
-    SysCtlDelay(13333333);
+    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x20);   //LED ON
+    SysCtlDelay(13333333);                        // Approx 1 sec delay
+    GPIOPinWrite(GPIO_PORTA_BASE, 0XF0 , 0x00);   //LED OFF
+    SysCtlDelay(13333333);                        // Approx 1 sec delay
     }
 }
 
@@ -49,8 +49,8 @@ int main(void)
     Board_initGeneral();
     Board_initGPIO();
 
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE ,0xF0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);  // Configure GPIO Periheral of Tiva Microcontroller using Tivaware library File.
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE ,0xF0); // Configuring GPIO Pins of the Peripheral.
 
     BIOS_start();
 
