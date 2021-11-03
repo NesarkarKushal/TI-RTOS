854c2ddf3673d28943e44ef10d6665556ba73e82
NesarkarKushal <86586956+NesarkarKushal@users.noreply.github.com>
2021-11-03 09:46:29 +0530

    Super Loop Program

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+int main()
+{
+
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_XTAL_16MHZ|SYSCTL_USE_PLL | SYSCTL_OSC_MAIN);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE , 0X0F);
+
+    while(1)
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, 0X0F, 0x02);
+        SysCtlDelay(13333333);
+
+        GPIOPinWrite(GPIO_PORTF_BASE, 0X0F, 0x00);
+        SysCtlDelay(13333333);
+
+    }
+}
